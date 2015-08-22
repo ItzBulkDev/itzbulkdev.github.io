24939308dd3f49b7816e35181c5328d4a68cbb86
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-08-21 20:19:10 -0400

    index.html

---
+++ index.php
@@ -0,0 +1,7 @@
+<head>
+<title>ItzBulkDev XML</title>
+<meta name=viewport content="width=device-width, user-scalable=no">
+</head>
+<body>
+<a href="http://itzbulkdev.github.io/alt.xml">ItzBulkDev REPO</a>
+</body>
