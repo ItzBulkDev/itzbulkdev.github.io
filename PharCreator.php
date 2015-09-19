9b07811c88225d75248b0d910e54091da1721495
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-09-19 11:33:08 -0400

    Create PharCreator.php

---
+++ PharCreator.php
@@ -0,0 +1,13 @@
+<?php
+try {
+    $zipphar = new PharData('myphar.zip');
+    // convert it to the phar file format
+    // note that myphar.tar is *not* unlinked
+    $phar = $zipphar->convertToExecutable(Phar::PHAR); // creates myphar.phar
+    $phar->setStub($phar->createDefaultStub('cli.php', 'web/index.php'));
+    // creates myphar.phar.tgz
+    $compressed = $zipphar->convertToExecutable(Phar::TAR, Phar::GZ, '.phar.tgz');
+} catch (Exception $e) {
+    // handle the error here
+}
+?>
